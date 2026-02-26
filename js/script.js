2ed049e65ea6f1498c53ce256839ff1e922b2da9
JOYCE <joycellorera2@gmail.com>
2026-02-27 00:47:02 +0800

    resolved merge conflict

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
     const blogContainer = document.querySelector('.daily-blog');
     const blogEntries = [...blogContainer.querySelectorAll('.blog-content')];
@@ -8,4 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
             this.closest('.blog-content').classList.toggle('active');
         });
     });
+=======
+document.addEventListener("DOMContentLoaded", () => {
+    const blogContainer = document.querySelector('.daily-blog');
+    const blogEntries = [...blogContainer.querySelectorAll('.blog-content')];
+    blogEntries.reverse().forEach(entry => blogContainer.appendChild(entry));
+
+    document.querySelectorAll('.preview').forEach(el => {
+        el.addEventListener('click', function () {
+            this.closest('.blog-content').classList.toggle('active');
+        });
+    });
+>>>>>>> 048e119 (Add images)
 });
